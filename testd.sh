33cbcb163bff6c28bccb90a5d7fdaccd40f4e93d
bNobo <benoit.rocco@gmail.com>
2021-03-26 20:04:42 +0100

    delete testd.sh

--- testd.sh
+++
@@ -1,5 +0,0 @@
-#daemon --name "testd" -b ~/source/wind-forecast-pi/testd.out -l ~/source/wind-forecast-pi/testd.err -v -- /home/pi/source/wind-forecast-pi/testd.sh
-while true; do
-	echo 'hi'
-	sleep 10
-done
